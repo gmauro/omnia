11ce199ecbc1f9f703be3149957d46c41ecdbdfa
Gianmauro Cuccuru <gianmauro.cuccuru@fht.org>
2022-11-22 17:54:50 +0100

    add config path

--- omnia/cli/info.py
+++ omnia/cli/info.py
@@ -1,4 +1,4 @@
-from omnia import __appname__, __version__, log_file
+from omnia import __appname__, __version__, log_file, config_dir
 
 help_doc = """
 Show Omnia details
@@ -12,9 +12,7 @@ def make_parser(parser):
 def implementation(logger, args):
     print("{} {}\n".format(__appname__.capitalize(), __version__))
 
-    omnia_paths = {
-        "log file": log_file,
-    }
+    omnia_paths = {"log file": log_file, "config dir": config_dir}
     print("Paths: ")
     for k, v in omnia_paths.items():
         print("  {}: {}".format(k, v))
