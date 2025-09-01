b8b6c60222c7901824be7aefc51cb551f080eb97
Gianmauro Cuccuru <gianmauro.cuccuru@fht.org>
2025-09-01 14:31:56 +0200

    fix: use glob to recursively find all files matching the pattern for file registration.
    This mimics the behavior of the ls command when handling paths with and without wildcards.

--- src/omnia/cli/reg.py
+++ src/omnia/cli/reg.py
@@ -1,6 +1,4 @@
-import fnmatch
-import re
-from pathlib import Path
+import glob
 
 import click
 import cloup
@@ -15,68 +13,23 @@ Register files to a Omnia collection.
 """
 
 
-def check_file_existence(file_paths):
+def match_files(search_path: str) -> list[str]:
     """
-    Check if the given file paths exist.
-
-    Parameters:
-    file_paths (list): A list of file paths to check.
-
-    Returns:
-    dict: A dictionary with file paths as keys and a boolean value indicating existence.
-    """
-    existence_check = {}
-    for file_path in file_paths:
-        existence_check[file_path] = file_path.exists() and file_path.is_file()
-    return existence_check
-
-
-def match_files(directory: Path, pattern: str) -> list[Path]:
-    """
-    Match files using either regex or glob pattern.
+    Match files using a glob pattern.
 
     Args:
-        directory: Directory to search
-        pattern: Pattern (regex or glob)
+        search_path: String to search for files.
 
     Returns:
         List of matching file paths
     """
-    if validate_regex(pattern):
-        return get_file_paths_from_regex(pattern, directory)
-    elif any(c in pattern for c in ["*", "?", "[", "]"]):
-        return get_file_paths_from_glob(pattern, directory)
-    else:
-        return [Path(pattern)]
+    # Use glob to find all files matching the pattern recursively
+    files = glob.glob(search_path, recursive=True)
 
+    # Sort the files to match the behavior of `ls`
+    files.sort()
 
-def get_file_paths_from_regex(pattern: str, directory: Path) -> list[Path]:
-    """Find files matching a regex pattern."""
-    return [file_path for file_path in directory.rglob("*") if re.search(pattern, file_path.name)]
-
-
-def get_file_paths_from_glob(pattern: str, directory: Path) -> list[Path]:
-    """Find files matching a glob pattern."""
-    return [file_path for file_path in directory.rglob("*") if fnmatch.fnmatch(file_path.name, pattern)]
-
-
-def validate_regex(pattern: str) -> bool:
-    """Validate regex pattern."""
-    try:
-        re.compile(pattern)
-        return True
-    except re.error:
-        return False
-
-
-def validate_directory(path: str | Path) -> bool:
-    """Validate directory path."""
-    match path:
-        case str():
-            directory = Path(path)
-        case Path():
-            directory = path
-    return directory.exists() and directory.is_dir()
+    return files
 
 
 @cloup.command("reg", no_args_is_help=True, help=HELP_DOC)
@@ -88,21 +41,11 @@ def validate_directory(path: str | Path) -> bool:
 def reg(ctx, source, collection, skip_metadata_computation, force):
     """Register files into Omnia"""
 
-    directory = Path(source).parent
-    file_pattern = Path(source).name
-
-    if not validate_directory(directory):
-        print("Invalid search path. Please enter a valid directory path.")
-        return
-
-    file_paths = match_files(directory, file_pattern)
-
+    file_paths = match_files(source)
     if not file_paths:
         print("No files found matching the pattern.")
         return
-
-        # Check if the found files exist
-    existence_check = check_file_existence(file_paths)
+    print(f"{len(file_paths)} files to register")
 
     compute_metadata = not skip_metadata_computation
 
@@ -117,11 +60,7 @@ def reg(ctx, source, collection, skip_metadata_computation, force):
             _collection = dataset.mdb_obj
 
             # Print the results
-            for file_path, exists in existence_check.items():
-                if not exists:
-                    print(f"File: {file_path} does not exist. Skipping ...")
-                    continue
-
+            for file_path in file_paths:
                 pdo = PosixDataObject(path=file_path).map()
 
                 if not pdo:
@@ -144,5 +83,5 @@ def reg(ctx, source, collection, skip_metadata_computation, force):
                     else:
                         if not force:
                             print(f"File {file_path} already registered. Use --force to overwrite.")
-                            return
+                            continue
                         pdo.compute().update()
